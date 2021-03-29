85050309c9fec84d27d2b34faa3ea17ba3f8b747
Ihor139 <53405232+Ihor139@users.noreply.github.com>
2021-03-29 23:21:38 +0300

    Final changes

--- js/index.js
+++ js/index.js
@@ -6,17 +6,22 @@ $(document).ready(function() {
     let sideMenu = document.querySelector('.side-menu');
     let btnMenuItem2 = document.querySelectorAll('.menu-item-link');
     let btnMenuItem3 = document.querySelectorAll('.side-menu-list-1-item-link');
-    let searchForm = document.querySelectorAll('.search-form')
-    let menu2lv = document.querySelectorAll('.side-menu-list-1')
-    let menu3lv = document.querySelectorAll('.side-menu-list-2')
+    let searchForm = document.querySelectorAll('.search-form');
+    let menu2lv = document.querySelectorAll('.side-menu-list-1');
+    let menu3lv = document.querySelectorAll('.side-menu-list-2');
     let backToMenuBtn = document.querySelectorAll('.back-to-menu');
     let backToMainMenuBtn = document.querySelectorAll('.back-to-main-menu');
     let backToMenuLv2Btn = document.querySelectorAll('.back-menu-1');
-    let page = document.querySelector('.page-wrapper')
-    let productBtn = document.querySelector('.product')
-    let aboutBtn = document.querySelectorAll('.about')
-    let menuAbout = document.querySelectorAll('.menu-item-about')
-    let popup = document.querySelector('.popup')
+    let page = document.querySelector('.page-wrapper');
+    let productBtn = document.querySelector('.product');
+    let aboutBtn = document.querySelectorAll('.about');
+    let menuAbout = document.querySelectorAll('.menu-item-about');
+    let popup = document.querySelector('.popup');
+    let searchBtn = document.querySelector('.search-btn');
+    let englishBtn = document.querySelector('.english'),
+        ukrainianBtn = document.querySelector('.ukrainian'),
+        polishBtn = document.querySelector('.polish'),
+        russianBtn = document.querySelector('.russian');
 
     // Footer position
 
@@ -105,6 +110,10 @@ $(document).ready(function() {
         $(searchForm).removeClass('active')
         $(menu2lv).removeClass('active');
         $(menu3lv).removeClass('active');
+
+        $(ukrainianBtn).addClass('hidden');
+        $(polishBtn).addClass('hidden');
+        $(russianBtn).addClass('hidden');
     })
 
     //????????????????
@@ -142,6 +151,26 @@ $(document).ready(function() {
     })
 
 
+    //Focus input search
+
+    $(searchBtn).on('click', function() {
+        $('#search-input').focus();
+        $(headNav).addClass('active');
+        $(btnMenu).removeClass('active');
+        $(btnBack).addClass('active');
+        $(page).addClass('open-menu');
+    })
+
+
+    //Focus languages
+
+    $(englishBtn).on('click', function() {
+        $(ukrainianBtn).toggleClass('hidden');
+        $(polishBtn).toggleClass('hidden');
+        $(russianBtn).toggleClass('hidden');
+    })
+
+
     // jquery animate
 
     let item1 = document.querySelector('.jquery-item-1'),
@@ -151,18 +180,18 @@ $(document).ready(function() {
     const items = [item1, item2, item3];
 
     function incrementHeight(start, step, delay, stepDelay) {
-        // let start = 50;
-        // let step = 25;
-        // let delay = 1000;
-        // let stepDelay = 1000;
-        for (let i = 0; i < items.length; i++) {
-            setTimeout(() => {
-                $(items[i]).animate({
-                    height: start + '%',
-                }, 1000);
-                start += step;
-            }, delay);
-            delay += stepDelay;
+        if (items.length > 0) {
+            for (let i = 0; i < items.length; i++) {
+                setTimeout(() => {
+                    $(items[i]).animate({
+                        height: start + '%',
+                    }, 1000);
+                    start += step;
+                }, delay);
+                delay += stepDelay;
+            }
+        } else {
+            console.log('error');
         }
     }
     incrementHeight(50, 25, 1000, 1000);
